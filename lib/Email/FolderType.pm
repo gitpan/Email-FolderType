dc0565664ea4eb45ac5da55c593474ab3920f00a
Richard Clamp <richardc@unixbeard.net>
2003-05-02 12:11:45 +0000

    Import of RCLAMP/Email-FolderType-0.3 from CPAN.
    gitpan-cpan-distribution: Email-FolderType
    gitpan-cpan-version:      0.3
    gitpan-cpan-path:         RCLAMP/Email-FolderType-0.3.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- lib/Email/FolderType.pm
+++ lib/Email/FolderType.pm
@@ -7,7 +7,7 @@ require Exporter;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(folder_type);
 
-$VERSION = '0.2';
+$VERSION = '0.3';
 
 =head1 NAME
 
@@ -20,32 +20,31 @@ Email::FolderType - return type of a mail folder
 
   print folder_type "~/mymbox";     # prints 'Mbox'
   print folder_type "~/a_maildir/"; # prints 'Maildir'
-  print folder_type "some_mh/.";    # prints 'MH' 	
+  print folder_type "some_mh/.";    # prints 'MH'
 
 
 =head1 SUBROUTINES
 
 =head2 folder_type <file path>
 
-Automatically detects what type of mail archive the 
+Automatically detects what type of mail archive the
 file path is and returns the name.
 
 =cut
 
-sub folder_type ($) 
+sub folder_type ($)
 {
     my $folder = shift;
-    return unless -e $folder;
     return "Maildir" if $folder =~ m{/$};
     return "MH"      if $folder =~ m{/\.$};
     return "Maildir" if -d $folder;
     return "Mbox";
 }
- 
+
 =head1 DESCRIPTION
 
-Provides a utility module for detecting the type of 
-a given mail box. 
+Provides a utility module for detecting the type of
+a given mail box.
 
 =head1 BUGS
 
@@ -57,11 +56,11 @@ Simon Wistow <simon@thegestalt.org>
 
 =head1 COPYING
 
-(C)opyright 2003, Simon Wistow 
+(C)opyright 2003, Simon Wistow
 
-Distributed under the same terms as Perl itself. 
+Distributed under the same terms as Perl itself.
 
-This software is under no warranty and will probably ruin your life, kill your friends, burn your house and bring about the apocalypse 
+This software is under no warranty and will probably ruin your life, kill your friends, burn your house and bring about the apocalypse
 
 
 =head1 SEE ALSO
