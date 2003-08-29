55a729e5ee7afc7bf8ab30fa208813ea9e805323
Richard Clamp <richardc@unixbeard.net>
2003-08-29 12:35:52 +0000

    Import of RCLAMP/Email-FolderType-0.6 from CPAN.
    gitpan-cpan-distribution: Email-FolderType
    gitpan-cpan-version:      0.6
    gitpan-cpan-path:         RCLAMP/Email-FolderType-0.6.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- lib/Email/FolderType.pm
+++ lib/Email/FolderType.pm
@@ -1,54 +1,65 @@
-package Email::FolderType;
-
 use strict;
+package Email::FolderType;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
 require Exporter;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(folder_type);
 
-$VERSION = '0.5';
+$VERSION = '0.6';
 
 =head1 NAME
 
-Email::FolderType - return type of a mail folder
+Email::FolderType - determine the type of a mail folder
 
 =head1 SYNOPSIS
 
   use Email::FolderType qw(folder_type);
 
-
   print folder_type "~/mymbox";     # prints 'Mbox'
   print folder_type "~/a_maildir/"; # prints 'Maildir'
   print folder_type "some_mh/.";    # prints 'MH'
-
-
-=head1 SUBROUTINES
-
-=head2 folder_type <file path>
-
-Automatically detects what type of mail archive the
-file path is and returns the name.
-
-=cut
-
-sub folder_type ($)
-{
-    my $folder = shift;
-    return "Maildir" if $folder =~ m{/$};
-    return "MH"      if $folder =~ m{/\.$};
-    return "Maildir" if -d $folder;
-    return "Mbox";
-}
+  print folder_type "an_archive//"; # prints 'Ezmlm'
 
 =head1 DESCRIPTION
 
-Provides a utility module for detecting the type of
-a given mail box.
+Provides a utility subroutine for detecting the type of a given mail
+folder.
 
-=head1 BUGS
+=head1 SUBROUTINES
 
-Currently only handles mh, mbox and maildir
+=head2 folder_type <path>
+
+Automatically detects what type of mail folder the path refers to and
+returns the name of that type.
+
+It primarily bases the type on the suffix of the path given.
+
+  Suffix | Type
+ --------+---------
+  /      | Maildir
+  /.     | MH
+  //     | Ezmlm
+
+In case of no known suffix it checks for a known file structure.  If
+that doesn't work out it defaults to C<Mbox>.
+
+=cut
+
+sub folder_type ($) {
+    my $folder = shift;
+
+    return "Ezmlm"   if $folder =~ m{//$};
+    return "Maildir" if $folder =~ m{/$};
+    return "MH"      if $folder =~ m{/\.$};
+    return "Maildir" if -d "$folder/cur";
+    return "Ezmlm"   if -d "$folder/archive";
+    # XXX return "MH" if -e WHAT?;
+    return "Mbox";
+}
+
+1;
+__END__
 
 =head1 AUTHOR
 
@@ -56,12 +67,12 @@ Simon Wistow <simon@thegestalt.org>
 
 =head1 COPYING
 
-(C)opyright 2003, Simon Wistow
+(C) Copyright 2003, Simon Wistow
 
 Distributed under the same terms as Perl itself.
 
-This software is under no warranty and will probably ruin your life, kill your friends, burn your house and bring about the apocalypse
-
+This software is under no warranty and will probably ruin your life,
+kill your friends, burn your house and bring about the apocalypse.
 
 =head1 SEE ALSO
 
